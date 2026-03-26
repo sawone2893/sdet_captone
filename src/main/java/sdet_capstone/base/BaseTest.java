10e84bfe4160fc74cdda6f8635669d4a625d7a8a
Shabbir Rayeen <sawone2893@gmail.com>
2026-03-26 09:52:41 +0530

    Updated TestNG Annotations

--- src/main/java/sdet_capstone/base/BaseTest.java
+++ src/main/java/sdet_capstone/base/BaseTest.java
@@ -1,34 +1,23 @@
 package sdet_capstone.base;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
 import sdet_capstone.utils.PropertyFileManager;
 
 public class BaseTest {
-	
-	@BeforeTest
+
+	@BeforeClass
 	public void browserSetup() {
 		BaseClass.launchBrowser(PropertyFileManager.getPropertyValue("BROWSER_TYPE"));
-	}
-	
-	@BeforeMethod
-	public void appLaunch() {
 		BaseClass.startApp(PropertyFileManager.getPropertyValue("APP_URL"));
 		BaseClass.implicitWait(Integer.parseInt(PropertyFileManager.getPropertyValue("DEFAULT_IMPLICIT_WAIT")));
 	}
-	
-	@AfterMethod
+
+	@AfterClass
 	public void closeApp() {
 		BaseClass.closeAppCurrentTabWindow();
-	}
-	
-	@AfterTest
-	public void terminateBrowser() {
 		BaseClass.closeApp();
 	}
-	
 
 }
